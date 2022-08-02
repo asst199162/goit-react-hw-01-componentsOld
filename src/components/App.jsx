c58eb31fcfb898e176af6e07fe7a75f85db25a1d
asst199162 <asst199162@gmail.com>
2022-08-02 13:42:32 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import PropTypes from 'prop-types';
+import {Profile} from 'components/profile/profile'
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template2
-    </div>
+    
+    <>
+        <Profile/>
+    </>
+    
   );
 };
