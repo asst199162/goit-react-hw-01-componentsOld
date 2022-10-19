f832a719dc645c97a677ffa6f42d23709b13df88
asst199162 <asst199162@gmail.com>
2022-10-19 18:22:33 +0300

    prototypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { ProfileCard } from 'components/profile/profile'
 import user from 'components/user.json'
 export const App = () => {
