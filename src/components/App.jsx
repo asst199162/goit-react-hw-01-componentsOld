ad16ecb629bd7d047eeed25eb27dbd4ac3d81c83
asst199162 <asst199162@gmail.com>
2022-10-19 22:11:25 +0300

    prototypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 //import PropTypes from 'prop-types';
 import { ProfileCard } from 'components/profile/profile'
 import user from 'components/user.json'
+
+import { Statistics } from './Statistics/statistics';
+import data from 'components/data.json';
+
+import { FriendList } from './FriendList/freind';
+import friends from 'components/friends.json'
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from 'components/transactions.json'
+
+
 export const App = () => {
   return (
     
     <>
-        <ProfileCard users = {user}/>
+      <ProfileCard users={user} />
+      <Statistics data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions = {transactions}/>
+      
     </>
     
   );
---
+++ src/components/FriendList/freind.jsx
@@ -0,0 +1,10 @@
+export const FriendList = ({ }) =>
+{
+    return <>
+    <li class="item">
+        <span class="status"></span>
+        <img class="avatar" src="" alt="User avatar" width="48" />
+        <p class="name"></p>
+    </li>
+    </>
+}
